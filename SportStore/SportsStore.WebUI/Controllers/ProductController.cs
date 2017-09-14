b98ec154b669c72318cf48b59a63cc0178672ec7
PhuNV <nguyenphu1312@gmail.com>
2017-09-15 05:29:41 +0700

    Chapter 7: Hoan thien chuong 7, bao gom tao project,tao database,chinh sua view su dung bootstrap

--- SportStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using SportStore.Domain.Abstract;
+﻿using SportsStore.WebUI.Models;
+using SportStore.Domain.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +11,29 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductsRepository repository;
+        public int PageSize = 4;
         public ProductController(IProductsRepository productRepository)
         {
             this.repository = productRepository;
         }
         //
         // GET: /Product/
-        public ActionResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(model);
         }
 	}
 }
---
+++ SportStore/SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SportStore.Domain.Entities;
+using System.Collections.Generic;
+
+namespace SportsStore.WebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
